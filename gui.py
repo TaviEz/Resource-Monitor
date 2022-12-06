529ff1cabba7e907355305971288a177a377c82c
TaviEz <tavicoste13@gmail.com>
2022-12-06 19:06:37 +0200

    Added more details on the RAM usage section and also added a disk usage text box where the user can see his partitions, the total space and the free space

--- gui.py
+++ gui.py
@@ -1,7 +1,10 @@
+import platform
 import tkinter as tk
 import os
 import subprocess
 import psutil
+import cpuinfo
+import tabulate
 from main import startGraph
 
 
@@ -16,14 +19,40 @@ def updateCPUUsage():
     CPUusageLabel.config(text="CPU usage: " + str(psutil.cpu_percent()) + "%")
     CPUusageLabel.after(1000,updateCPUUsage)
 
-def updateCPUfreq():
-    CPUfreqLabel.config(text="Frequency: " + str(psutil.cpu_freq().current))
-    CPUfreqLabel.after(1000,updateCPUfreq)
-
 def updateRAMUsage():
-    RAMlabel.config(text="RAM usage: " + str(psutil.virtual_memory().percent) + "%")
+    total = byteToGigabyte(psutil.virtual_memory().total)
+    used = byteToGigabyte(psutil.virtual_memory().used)
+    RAMlabel.config(text="RAM: " + used + "GB/" + total + "GB " + str(psutil.virtual_memory().percent) + "%")
     RAMlabel.after(1000, updateRAMUsage)
 
+
+data = psutil.disk_partitions()
+#device = name of the partition ex: C\\  D\\ etc
+def get_partition_names():
+    return [i.device for i in data]
+
+def disk_info(device_name):
+    disk_info = {}
+    usage = psutil.disk_usage(device_name)
+    disk_info['Device'] = device_name
+    disk_info['Total'] = f"{byteToGigabyte(usage.total)} GB"
+    disk_info['Used'] = f"{byteToGigabyte(usage.used)} GB"
+    disk_info['Free'] = f"{byteToGigabyte(usage.free)} GB"
+    disk_info['Percent'] = f"{usage.percent} %"
+    return disk_info
+
+def display_all_partitions():
+    info = []
+
+    for i in get_partition_names():
+        info.append(disk_info(i))
+
+    info_values = [i.values() for i in info]
+    info_tabulated = tabulate.tabulate(info_values, headers=info[0].keys())
+    return info_tabulated
+
+def byteToGigabyte(bytes):
+    return '{0:.2f}'.format(bytes/1000000000)
 
 root = tk.Tk()
 root.state("zoomed")
@@ -60,21 +89,32 @@ DiskusageBtn = tk.Button(homeFrame, text="Disk usage",padx=10, pady=5, fg="white
 DiskusageBtn.grid(row=1, column=3)
 
 
+
+CPUplatformLabel = tk.Label(homeFrame, text=platform.processor(), padx=10,pady=5)
+CPUplatformLabel.grid(row=2, column=1)
+
+CPUnameLabel = tk.Label(homeFrame, text=cpuinfo.get_cpu_info()["brand_raw"], padx=10, pady=5)
+CPUnameLabel.grid(row=3, column=1)
+
+CPUfreqLabel = tk.Label(homeFrame, text=str(psutil.cpu_freq().current), padx=10, pady=5)
+CPUfreqLabel.grid(row=4, column=1,pady=10)
+
+
 CPUusageLabel = tk.Label(homeFrame, text="", padx=10, pady=5)
-CPUusageLabel.grid(row=2, column=1, pady=10)
+CPUusageLabel.grid(row=5, column=1, pady=10)
 #CPUlabel.after(2000, lambda:updateText(CPUlabel, "CPU " + displayCPU()))
 updateCPUUsage()
 
-CPUfreqLabel = tk.Label(homeFrame, text="", padx=10, pady=5)
-CPUfreqLabel.grid(row=3, column=1,pady=10)
-updateCPUfreq()
-
 RAMlabel = tk.Label(homeFrame, text="", padx=10, pady=5)
-RAMlabel.grid(row=3, column=1, pady=10)
+RAMlabel.grid(row=6, column=1, pady=10)
 updateRAMUsage()
 
+diskLabel = tk.Label(homeFrame, text="Disk usage")
+diskLabel.grid(row=7, column=1, pady=10)
 
-
+textArea = tk.Text(homeFrame, bg="#00311C", fg='yellow',width=50, height=5)
+textArea.grid(row=8, column=1, pady=10)
+textArea.insert(tk.END, display_all_partitions())
 
 ### CPU frame
 cpuFrame_title = tk.Label(cpuFrame, text='CPU', bg='yellow')
--- test.py
+++ test.py
@@ -1,3 +1,56 @@
+import platform
+import cpuinfo
 import psutil
+import tabulate
+def byteToGigabyte(bytes):
+    return '{0:.2f}'.format(bytes/1000000000)
 
-print(str(psutil.cpu_freq()))
+data = psutil.disk_partitions()
+def get_device_names():
+    return [i.device for i in data]
+
+print(get_device_names())
+# print(details(get_device_names()[0]))
+# print(psutil.disk_usage("C:\\"))
+# print(byteToGigabyte(psutil.disk_usage("C:\\").total))
+# print(byteToGigabyte(psutil.disk_usage("C:\\").used))
+#
+#
+# info = details("C:\\")
+# print(info)
+# print(info[0])
+def get_partition_names():
+    return [i.device for i in data]
+def disk_info(device_name):
+    disk_info = {}
+    usage = psutil.disk_usage(device_name)
+    disk_info['Device'] = device_name
+    disk_info['Total'] = f"{byteToGigabyte(usage.total)} GB"
+    disk_info['Used'] = f"{byteToGigabyte(usage.used)} GB"
+    disk_info['Free'] = f"{byteToGigabyte(usage.free)} GB"
+    disk_info['Percent'] = f"{usage.percent} %"
+    return disk_info
+
+def all_partitions():
+    all = []
+    for i in get_device_names():
+        all.append(disk_info(i))
+    return all
+def display_all_partitions():
+    info = []
+
+    for i in get_partition_names():
+        info.append(disk_info(i))
+
+    info_values = [i.values() for i in info]
+    info_tabulated = tabulate.tabulate(info_values, headers=info[0].keys())
+    return info_tabulated
+
+print(display_all_partitions())
+#
+# print(disk_info("C:\\"))
+# print(disk_info("E:\\"))
+info = all_partitions()
+info_values = [i.values() for i in info]
+info_tabulated = tabulate.tabulate(info_values, headers=info[0].keys())
+print(info_tabulated)
